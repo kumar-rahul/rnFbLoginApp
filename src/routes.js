9b706ca22948a7d1822947cb6319f07536537368
kumar rahul <rahul.k@greatlearning.in>
2019-11-29 17:41:42 +0530

    fonts

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import { Text, View, StyleSheet } from 'react-native';
+import { Fonts } from './assets/fonts'
+
 
 class Routes extends Component {
     constructor(props) {
@@ -20,7 +22,7 @@ class Routes extends Component {
     render() {
         return (
             <View style={styles.container}>
-                <Text>hello world</Text>
+                <Text style={styles.text}>hello world</Text>
             </View>
         );
     }
@@ -36,6 +38,10 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
     },
+    text: {
+        fontFamily: Fonts.LatoBold,
+        fontSize: 30
+    },    
 });
 
 const mapStateToProps = createStructuredSelector({});
